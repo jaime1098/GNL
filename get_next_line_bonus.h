1068bb887dc152f4670252125068d68d15ebca55
Jaime Oltra Riera <joltra-r@c1r5s2.42madrid.com>
2024-04-30 16:25:05 +0200

    Get_Next_Line

---
+++ get_next_line_bonus.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joltra-r <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/21 12:51:38 by joltra-r          #+#    #+#             */
+/*   Updated: 2023/11/21 12:51:41 by joltra-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <fcntl.h>	
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <limits.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+int		ft_strlen(char *str);
+char	*ft_strchr(char *str, int c);
+
+#endif
